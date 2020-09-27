73bc94d3fbd9d536647ebd07d9afdead55fc0a7e
Kentaro Ueda <kentaro.ueda.kentaro@gmail.com>
2020-09-27 22:48:20 +0900

    Updates

---
+++ precache-manifest.03f7e90d83c144ca87b0ea95ed2be3e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99ca4ec1cec913cf22be143e71245a19",
+    "url": "/web_rtc_react/index.html"
+  },
+  {
+    "revision": "e3333b841f3401415d13",
+    "url": "/web_rtc_react/static/css/main.cffdc1f4.chunk.css"
+  },
+  {
+    "revision": "274ad38f4f22c8dcf34f",
+    "url": "/web_rtc_react/static/js/2.a8e3f6df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/web_rtc_react/static/js/2.a8e3f6df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3333b841f3401415d13",
+    "url": "/web_rtc_react/static/js/main.94eabfe4.chunk.js"
+  },
+  {
+    "revision": "3471f68233fb1d0c30c7",
+    "url": "/web_rtc_react/static/js/runtime-main.e012281c.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/web_rtc_react/precache-manifest.03f7e90d83c144ca87b0ea95ed2be3e4.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/web_rtc_react/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
